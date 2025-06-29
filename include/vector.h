01710e5d5909b477bf357557856dbdfc7096c0a8
apynu <apynu@duck.com>
2025-06-29 13:59:57 +0200

    "i am stupid" -charles leclerc

--- include/vector.h
+++ include/vector.h
@@ -8,14 +8,12 @@ public:
   using std::vector<T>::vector;
   using std::vector<T>::operator=;
 
-  // erase an element at some index via an int
-  // WARN: this erase method is very unperformant
+  // erase an element at some index
   void erase(int index);
 
   // erase all elements in the vector
   // WARN: this erase method is very unperformant
-  //void erase(T element);
-
+  // void erase(T element);
 };
 } // namespace aul
 
